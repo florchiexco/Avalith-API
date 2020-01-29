59877db6f0a74cfcd81031eb6ec5851fee3f3a7f
florchiexco <florchiexco@gmail.com>
2020-01-29 20:45:24 -0300

    funcion de añadir imagen al documento

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Lectura de la API
+
 async function readAPIcats(){
     try{
         var response= await fetch("https://cat-fact.herokuapp.com/facts");
@@ -15,16 +17,18 @@ function traerDatosFacts(data){
     arregloTotalFacts=data;
     console.log(arregloTotalFacts);
 }
-readAPIcats().then(traerDatosFacts);
 
+//Manejo de las imágenes
 
-var fact;
+function numeroAleatorio(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+  }
 
-function primerFact(){
-    fact=arregloTotal[0];
-    console.log(fact)
+var img;
+function añadirImagen(){
+    img= document.createElement("img");
+    img.setAttribute("src", "img/" + numeroAleatorio(1,10)+ ".jpg");
+    img.setAttribute("height","250");
+    document.getElementById("contenedor").appendChild(img);
 }
-
-readAPIcats().then(primerFact);
-
-
+document.addEventListener("DOMContentLoaded", añadirImagen);
